be22416d9df63c4ed6e2d8b5cd34520291480943
강준하 <pointjunha@naver.com>
2017-08-05 20:42:15 +0900

    아까 잘못

--- node.server.js
+++ node.server.js
@@ -1,9 +1,9 @@
 var http = require('http');
-var server port를 process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 
 http.createServer(function (request, response) {
 	response.writeHead(200, {'Content-Type': 'text/html'});
 	response.end('<h1>Hello world!</h1>');
 }).listen(port, function() {
-	console.log('Server running at http://127.0.0.1:8084/');
+	console.log('Server running at http://127.0.0.1:' + port);
 });
