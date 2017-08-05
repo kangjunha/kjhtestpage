11260b81affa6bbbf43d300e7fd778c74b04e716
강준하 <pointjunha@naver.com>
2017-08-05 20:41:14 +0900

    port number 방식 변

--- node.server.js
+++ node.server.js
@@ -1,8 +1,9 @@
 var http = require('http');
+var server port를 process.env.PORT || 3000;
 
 http.createServer(function (request, response) {
 	response.writeHead(200, {'Content-Type': 'text/html'});
 	response.end('<h1>Hello world!</h1>');
-}).listen(8085, function() {
+}).listen(port, function() {
 	console.log('Server running at http://127.0.0.1:8084/');
 });
