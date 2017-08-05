528e70ed18baf5e9e41508fcf957337dc3551982
강준하 <pointjunha@naver.com>
2017-08-05 23:58:38 +0900

    changed port number

--- web.js
+++ web.js
@@ -1,11 +1,11 @@
 var http = require('http');
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 
 http.createServer(function (request, response) {
 	response.writeHead(200, {'Content-Type': 'text/html'});
 	response.end('<h1>Hello world!</h1>');
-}).listen(port, /*function() {
+}).listen(port, function() {
 	console.log('Server running at http://127.0.0.1:' + port);
-}*/);
+});
 
-console.log('Server running at http://127.0.0.1:' + port);
+//console.log('Server running at http://127.0.0.1:' + port);
