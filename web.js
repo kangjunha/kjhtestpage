7907022c08c88e1debe36176eb71fcdaf997c16c
강준하 <pointjunha@naver.com>
2017-08-06 01:06:37 +0900

    으아아

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var port = process.env.PORT || 8080;
 
 http.createServer(function (request, response) {
 	response.writeHead(200, {'Content-Type': 'text/html'});
-	response.end('<h1>Pray for Outsider...</h1>');
+	response.end('<h1>Kang Junha\'s Page</h1>');
 }).listen(port, function() {
 	console.log('Server running at http://127.0.0.1:' + port);
 });
