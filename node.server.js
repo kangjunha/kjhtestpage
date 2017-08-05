b0a659d4515079a6edabfe999aef36c8e167b6b4
강준하 <pointjunha@naver.com>
2017-08-05 19:56:14 +0900

    change port number

--- node.server.js
+++ node.server.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (request, response) {
 	response.writeHead(200, {'Content-Type': 'text/html'});
 	response.end('<h1>Hello world!</h1>');
-}).listen(8084, function() {
+}).listen(8085, function() {
 	console.log('Server running at http://127.0.0.1:8084/');
 });
