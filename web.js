01a770d24686f0979b393e92ecb4befc8f629539
강준하 <pointjunha@naver.com>
2017-08-05 22:45:02 +0900

    changed listen function arguments

--- web.js
+++ web.js
@@ -4,6 +4,8 @@ var port = process.env.PORT || 3000;
 http.createServer(function (request, response) {
 	response.writeHead(200, {'Content-Type': 'text/html'});
 	response.end('<h1>Hello world!</h1>');
-}).listen(port, function() {
+}).listen(port, /*function() {
 	console.log('Server running at http://127.0.0.1:' + port);
-});
+}*/);
+
+console.log('Server running at http://127.0.0.1:' + port);
