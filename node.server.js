3e6a3d4cedf81251049a611da5ab3c85930eb139
강준하 <pointjunha@naver.com>
2017-08-05 20:36:21 +0900

    restart

--- node.server.js
+++ node.server.js
@@ -1,12 +1,8 @@
-var express = require('express');
+var http = require('http');
 
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+http.createServer(function (request, response) {
+	response.writeHead(200, {'Content-Type': 'text/html'});
+	response.end('<h1>Hello world!</h1>');
+}).listen(8085, function() {
+	console.log('Server running at http://127.0.0.1:8084/');
 });
