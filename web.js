2b33ad94b9b8b661fc65612c0ec5206db2728a7c
Junha <junha@Junhaui-MacBook-Pro.local>
2018-05-31 22:03:16 +0900

    for homework

--- web.js
+++ web.js
@@ -4,18 +4,17 @@ var url = require('url');
 var port = process.env.PORT || 8080;
 
 // 서버 생성
-http.createServer(function (request, response) {  
+http.createServer(function (request, response) {
    // URL 뒤에 있는 디렉토리/파일이름 파싱
    var pathname = url.parse(request.url).pathname;
-   
-   
+
    console.log("Request for " + pathname + " received.");
-   
+
    // 파일 이름이 비어있다면 index.html 로 설정
    if(pathname == "/") {
        pathname = "/index.html";
    }
-   
+
    // 파일을 읽기
    fs.readFile(pathname.substr(1), function (err, data) {
       if (err) {
@@ -25,18 +24,18 @@ http.createServer(function (request, response) {
          // Content Type: text/plain
          response.writeHead(404, {'Content-Type': 'text/html'});
       }
-      else {	
-         // 페이지를 찾음	  
+      else {
+         // 페이지를 찾음
          // HTTP Status: 200 : OK
          // Content Type: text/plain
-         response.writeHead(200, {'Content-Type': 'text/html'});	
-         
+         response.writeHead(200, {'Content-Type': 'text/html'});
+
          // 파일을 읽어와서 responseBody 에 작성
-         response.write(data.toString());		
+         response.write(data.toString());
       }
       // responseBody 전송
       response.end();
-   });   
+   });
 }).listen(port);
 
 
