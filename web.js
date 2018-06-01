da0946fdd4dd91f936deef88b91cfb9ca2b09e6b
Junha <junha@Junhaui-MacBook-Pro.local>
2018-06-01 10:32:11 +0900

    g, n update part made

--- web.js
+++ web.js
@@ -2,11 +2,22 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var port = process.env.PORT || 8080;
+var node_jsdom = require('node-jsdom');
+var jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+const { window } = new JSDOM();
+const { document } = (new JSDOM('')).window;
+global.document = document;
+
+// var $ = jQuery = require('jquery')(window);
+
 
 // 서버 생성
 http.createServer(function (request, response) {
    // URL 뒤에 있는 디렉토리/파일이름 파싱
    var pathname = url.parse(request.url).pathname;
+   var schedule = require('node-schedule');
+
 
    console.log("Request for " + pathname + " received.");
 
@@ -33,6 +44,24 @@ http.createServer(function (request, response) {
          // 파일을 읽어와서 responseBody 에 작성
          response.write(data.toString());
       }
+      var job = schedule.scheduleJob('30 * * * * *', function(){
+        node_jsdom.env(
+          "index.html",
+          ["http://code.jquery.com/jquery-3.3.1.min.js"],
+          function(errors, window){
+            try {
+              var primeList = [101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199];
+              var g = Math.floor((Math.random() * 100) + 1);
+              var n = primeList[Math.floor(Math.random() * primeList.length)];
+              fs.writeFileSync('gn_value.txt', g + '\n' + n, 'utf-8');
+              // console.log('WRITE DONE!');
+            }
+            catch(e) {
+              console.log(e);
+            }
+          }
+        );
+      });
       // responseBody 전송
       response.end();
    });
